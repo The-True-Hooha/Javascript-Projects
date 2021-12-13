0253ead2a46b0cf7351d2fb3fa5dee8eab864b71
David Ogar <owogogahhero@outlook.com>
2021-12-13 15:01:25 +0100

    completed the image slider project

--- image slider/script.js
+++ image slider/script.js
@@ -1,16 +1,32 @@
 const Next = document.querySelector(".Next")
 const Previous = document.querySelector(".Previous")
-const container = document.querySelector("images");
+const container = document.querySelector(".images");
 
 let counter = 0;
 
 Next.addEventListener("click", nextSlide);
 Previous.addEventListener("click", prevSlide);
 
+//inputs the next button function
 function nextSlide(){
-    counter ++;
+    //animation to the image slider
+    container.animate([{opacity: "0.1"}, {opacity: "1.0"}], {duration: 1000, fill: "forwards"})
+    if(counter === 6){
+        counter = -1;
+    }
+    counter++;
 
-    container.style.backgroundImage = 
- //   "url{images/toa-heftiba-uKRat_CK080-unsplash.jpg-$)
-    {counter}.jpg"
+    container.style.backgroundImage = `url(images/bcg-${counter}.jpg`
+}
+
+//inputs the previous button function
+function prevSlide(){
+    //animation to the image slider
+    container.animate([{opacity: "0.1"}, {opacity: "1.0"}], {duration: 1000, fill: "forwards"})
+    if(counter === 0){
+        counter = 7;
+    }
+    counter--;
+
+    container.style.backgroundImage = `url(images/bcg-${counter}.jpg`
 }
