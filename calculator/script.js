70304b2dcf33f17d7e23c8234e230022ef4496bc
David Ogar <owogogahhero@outlook.com>
2021-12-14 13:08:16 +0100

    add the clock project, which is not yet completed. Clock isn't displaying

--- calculator/script.js
+++ calculator/script.js
@@ -1,9 +1,29 @@
-const btns = document.querySelectorAll(".btn");
+const btns = document.querySelectorAll(".btn-grey");
 const screen = document.querySelector(".screen");
 const equalBtn = document.querySelector(".btn-equal")
 const clearBtn = document.querySelector(".btn-clear");
 
 
-for (let i = 0; i<btns.length; i++){
-    btns[i].addEventListener("click", function())
+//a for loop that ensures all the button can be clicked and displays on the screen
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function (){
+        let number = btns[i].getAttribute("data-num");
+        if(screen.value += number){
+            console.log("yes it is working");
+        }else{
+            console.log(err);
+        }
+    })
 }
+
+//displays the equal button to the screen
+equalBtn.addEventListener("click", function(){
+    let value = eval(screen.value);
+    screen.value = value;
+})
+
+//deletes any value when the clear button is clicked
+clearBtn.addEventListener("click", function (){
+    screen.value = "";
+})
+
