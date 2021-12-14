a7516ce4426b1137c0914909552aacbc47800c97
David Ogar <owogogahhero@outlook.com>
2021-12-14 09:29:24 +0100

    add a new project, midway in the calculator project

---
+++ calculator/script.js
@@ -0,0 +1,9 @@
+const btns = document.querySelectorAll(".btn");
+const screen = document.querySelector(".screen");
+const equalBtn = document.querySelector(".btn-equal")
+const clearBtn = document.querySelector(".btn-clear");
+
+
+for (let i = 0; i<btns.length; i++){
+    btns[i].addEventListener("click", function())
+}
